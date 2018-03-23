4d538c0f4ce0f650639d352509b81428fbeb350f
Tien <globaldevil12@gmail.com>
2018-03-23 15:02:19 +0700

    “Client-Server-RPS”

---
+++ Client-sever-RPS/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Client_sever_RPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnScissor = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSelect = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnScissor
+            // 
+            this.btnScissor.Location = new System.Drawing.Point(58, 122);
+            this.btnScissor.Name = "btnScissor";
+            this.btnScissor.Size = new System.Drawing.Size(102, 98);
+            this.btnScissor.TabIndex = 0;
+            this.btnScissor.Text = "Kéo";
+            this.btnScissor.UseVisualStyleBackColor = true;
+            this.btnScissor.Click += new System.EventHandler(this.btnScissor_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.Location = new System.Drawing.Point(210, 122);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(100, 98);
+            this.btnPaper.TabIndex = 1;
+            this.btnPaper.Text = "Bao";
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnRock
+            // 
+            this.btnRock.Location = new System.Drawing.Point(359, 122);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(94, 98);
+            this.btnRock.TabIndex = 2;
+            this.btnRock.Text = "Búa";
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Your Selection";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Result";
+            // 
+            // txtSelect
+            // 
+            this.txtSelect.Location = new System.Drawing.Point(153, 29);
+            this.txtSelect.Name = "txtSelect";
+            this.txtSelect.Size = new System.Drawing.Size(191, 22);
+            this.txtSelect.TabIndex = 5;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(153, 292);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(191, 22);
+            this.txtResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 357);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtSelect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRock);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnScissor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnScissor;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSelect;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ Client-sever-RPS/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_sever_RPS
+{
+    public partial class Form1 : Form
+    {
+        byte[] data = new byte[1024];
+        IPEndPoint ipep = new IPEndPoint(
+                        IPAddress.Parse("127.0.0.1"), 9050);
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint sender = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+        EndPoint Remote;
+        string input;
+        public Form1()
+        {
+            InitializeComponent();
+            server.SendTo(data, data.Length, SocketFlags.None, ipep);
+            Remote = (EndPoint)sender;
+            data = new byte[1024];
+        }
+        public void kq(string input, string k)
+        {
+            if (input == "1")
+            {
+                if (k == "1")
+                {
+                    txtResult.Text = "Hòa";
+                }
+                if (k == "2")
+                {
+                    txtResult.Text = "Thắng";
+                }
+                if (k == "3")
+                {
+                    txtResult.Text = "Thua";
+                }
+            }
+            if (input == "2")
+            {
+                if (k == "1")
+                {
+                    txtResult.Text = "Thua";
+                }
+                if (k == "2")
+                {
+                    txtResult.Text = "Hòa";
+                }
+                if (k == "3")
+                {
+                    txtResult.Text = "Thắng";
+                }
+            }
+            if (input == "3")
+            {
+                if (k == "1")
+                {
+                    txtResult.Text = "Thắng";
+                }
+                if (k == "2")
+                {
+                    txtResult.Text = "Thua";
+                }
+                if (k == "3")
+                {
+                    txtResult.Text = "Hòa";
+                }
+            }
+        }
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            txtSelect.Text = "Bao";
+            input = "2";
+            server.SendTo(Encoding.ASCII.GetBytes(input), Remote);
+            data = new byte[1024];
+            int recv = server.ReceiveFrom(data, ref Remote);
+            string k = Encoding.ASCII.GetString(data, 0, recv);
+            kq(input, k);
+        }
+        private void btnScissor_Click(object sender, EventArgs e)
+        {
+            txtSelect.Text = "Kéo";
+            input = "1";
+            server.SendTo(Encoding.ASCII.GetBytes(input), Remote);
+            data = new byte[1024];
+            int recv = server.ReceiveFrom(data, ref Remote);
+            string k = Encoding.ASCII.GetString(data, 0, recv);
+            kq(input, k);
+        }
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            txtSelect.Text = "Búa";
+            input = "3";
+            server.SendTo(Encoding.ASCII.GetBytes(input), Remote);
+            data = new byte[1024];
+            int recv = server.ReceiveFrom(data, ref Remote);
+            string k = Encoding.ASCII.GetString(data, 0, recv);
+            kq(input, k);
+        }
+    }
+}
